2df7e26e4566dd06b15ceae6c221b5f988d34bbf
Antwan <antwansittard@gmail.com>
2024-03-20 12:10:11 +0100

    update

--- turtleRobot/turtleRobot.ino
+++ turtleRobot/turtleRobot.ino
@@ -1,32 +1,23 @@
-#define IN1 4
-#define IN2 5
-#define IN3 6
-#define IN4 7
- 
+#define IN1 5
+#define IN2 4
+#define pwm 6
 void setup()
 {
 pinMode(IN1, OUTPUT);
 pinMode(IN2, OUTPUT);
-pinMode(IN3, OUTPUT);
-pinMode(IN4, OUTPUT);
+pinMode(pwm, OUTPUT);
+Serial.begin(9600);
 }
  
 void loop()
 {
 // Rotate the Motor A clockwise
+digitalWrite(pwm,HIGH);
 digitalWrite(IN1, HIGH);
 digitalWrite(IN2, LOW);
-delay(2000);
-// Motor A
-digitalWrite(IN1, HIGH);
-digitalWrite(IN2, HIGH);
-delay(500);
-// Rotate the Motor B clockwise
-digitalWrite(IN3, HIGH);
-digitalWrite(IN4, LOW);
-delay(2000);
-// Motor B
-digitalWrite(IN3, HIGH);
-digitalWrite(IN4, HIGH);
-delay(500);
+Serial.println("rotate moter A");
+
+
+
+
 }
