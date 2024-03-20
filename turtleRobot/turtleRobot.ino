ea41dac03f51702360bb3085af240248da533883
Antwan <antwansittard@gmail.com>
2024-03-20 13:24:18 +0100

    update

--- turtleRobot/turtleRobot.ino
+++ turtleRobot/turtleRobot.ino
@@ -2,7 +2,7 @@
 #define LeftM2 6
 #define RightM1 7
 #define RightM2 8
-#define LeftPwm 4
+#define LeftPwm 3
 #define RightPwm 9
 #define EncoderLeftA 11
 #define EncoderLeftB 12
@@ -13,6 +13,8 @@ int aLastStateLeft;
 int aLastStateRight;
 int counterLeft = 0;
 int counterRight = 0; 
+int aStateLeft;
+int aStateRight;
   
 void setup()
 {
@@ -26,44 +28,51 @@ void setup()
   pinMode(EncoderLeftB, INPUT);
   pinMode(EncoderRightA, INPUT);
   pinMode(EncoderRightB, INPUT);
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
  
 void loop()
 {
 // Rotate the Motor A clockwise
-analogWrite(LeftPwm,255);
+while(counterLeft < 90){
+ analogWrite(LeftPwm,50);
 digitalWrite(LeftM1, HIGH);
 digitalWrite(LeftM2, LOW);
 ReadRotations();
+ } 
+ digitalWrite(LeftM1, HIGH);
+digitalWrite(LeftM2, HIGH);
+
 }
 
 void ReadRotations(){
 
-  int aState;
-  aState = digitalRead(EncoderLeftA);
-   if (aState != aLastStateLeft){     
-     if (digitalRead(EncoderLeftB) != aState) { 
-       counter ++;
-     } else {
-       counter --;
-     }
-     Serial.print("PositionRight: ");
-     Serial.println(counter);
-   } 
-   aLastStateLeft = aState;
 
-  aState = digitalRead(EncoderRighttA);
-   if (aState != aLastStateRight){     
-     if (digitalRead(EncoderRightB) != aState) { 
-       counter ++;
+  aStateLeft = digitalRead(EncoderLeftA);
+   if (aStateLeft != aLastStateLeft){     
+     if (digitalRead(EncoderLeftB) != aStateLeft) { 
+       counterLeft ++;
      } else {
-       counter --;
+       counterLeft --;
+     }
+     Serial.print("Positionleft: ");
+     Serial.println(counterLeft);
+   } 
+   aLastStateLeft = aStateLeft;
+
+   
+  aStateRight = digitalRead(EncoderRightA);
+   if (aStateRight != aLastStateRight){     
+     if (digitalRead(EncoderRightB) != aStateRight) { 
+       counterRight ++;
+     } else {
+       counterRight --;
      }
      Serial.print("PositionRight: ");
-     Serial.println(counter);
+     Serial.println(counterRight);
    } 
-   aLastStateRight = aState;
+   aLastStateRight = aStateRight;
+  
 
    
 }
