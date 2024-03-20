cc72bdf2c6b68876f27fd2537c4e66f72ec40441
Antwan <antwansittard@gmail.com>
2024-03-20 12:43:25 +0100

    added read capebillities

--- turtleRobot/turtleRobot.ino
+++ turtleRobot/turtleRobot.ino
@@ -1,23 +1,69 @@
-#define IN1 5
-#define IN2 4
-#define pwm 6
+#define LeftM1 5
+#define LeftM2 6
+#define RightM1 7
+#define RightM2 8
+#define LeftPwm 4
+#define RightPwm 9
+#define EncoderLeftA 11
+#define EncoderLeftB 12
+#define EncoderRightA 1
+#define EncoderRightB 2
+
+int aLastStateLeft;
+int aLastStateRight;
+int counterLeft = 0;
+int counterRight = 0; 
+  
 void setup()
 {
-pinMode(IN1, OUTPUT);
-pinMode(IN2, OUTPUT);
-pinMode(pwm, OUTPUT);
-Serial.begin(9600);
+  pinMode(LeftM1, OUTPUT);
+  pinMode(LeftM2, OUTPUT);
+  pinMode(RightM1, OUTPUT);
+  pinMode(RightM2, OUTPUT);
+  pinMode(LeftPwm, OUTPUT);
+  pinMode(RightPwm, OUTPUT);
+  pinMode(EncoderLeftA, INPUT);
+  pinMode(EncoderLeftB, INPUT);
+  pinMode(EncoderRightA, INPUT);
+  pinMode(EncoderRightB, INPUT);
+  Serial.begin(9600);
 }
  
 void loop()
 {
 // Rotate the Motor A clockwise
-digitalWrite(pwm,HIGH);
-digitalWrite(IN1, HIGH);
-digitalWrite(IN2, LOW);
-Serial.println("rotate moter A");
+analogWrite(LeftPwm,255);
+digitalWrite(LeftM1, HIGH);
+digitalWrite(LeftM2, LOW);
+ReadRotations();
+}
 
+void ReadRotations(){
 
+  int aState;
+  aState = digitalRead(EncoderLeftA);
+   if (aState != aLastStateLeft){     
+     if (digitalRead(EncoderLeftB) != aState) { 
+       counter ++;
+     } else {
+       counter --;
+     }
+     Serial.print("PositionRight: ");
+     Serial.println(counter);
+   } 
+   aLastStateLeft = aState;
 
+  aState = digitalRead(EncoderRighttA);
+   if (aState != aLastStateRight){     
+     if (digitalRead(EncoderRightB) != aState) { 
+       counter ++;
+     } else {
+       counter --;
+     }
+     Serial.print("PositionRight: ");
+     Serial.println(counter);
+   } 
+   aLastStateRight = aState;
 
+   
 }
